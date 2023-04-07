2f738c7f4baae7c8a5a8004fff300336ab9ea8d4
Huang Peiwen <peiwen.huang@bric.jp>
2023-04-07 13:50:36 +0900

    在Controller中追加Service並利用Service來對資料庫進行處理

--- MyFirstApplication/src/main/java/com/example/demo/app/inquiry/InquiryController.java
+++ MyFirstApplication/src/main/java/com/example/demo/app/inquiry/InquiryController.java
@@ -1,5 +1,8 @@
 package com.example.demo.app.inquiry;
 
+import java.time.LocalDateTime;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -10,9 +13,19 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.example.demo.entity.Inquiry;
+import com.example.demo.service.InquiryService;
+
 @Controller
 @RequestMapping("/inquiry")
 public class InquiryController {
+	
+	private final InquiryService inquiryService;
+	
+	@Autowired
+	public InquiryController(InquiryService inquiryService) {
+		this.inquiryService = inquiryService;  // 初始化 InquiryService
+	}
 	
 	@GetMapping("/form")
 	public String form(InquiryForm inquiryForm, Model model,
@@ -44,6 +57,13 @@ public class InquiryController {
 			model.addAttribute("title", "Inquiry Form");
 			return "inquiry/form";
 		}
+		// 資料庫處理 新增資料
+		Inquiry inquiry = new Inquiry();
+		inquiry.setName(inquiryForm.getName());
+		inquiry.setEmail(inquiryForm.getEmail());
+		inquiry.setContents(inquiryForm.getContents());
+		inquiry.setCreated(LocalDateTime.now());
+		inquiryService.save(inquiry);
 		redirectAttributes.addFlashAttribute("complete", "Registered!");/*關鍵識別字, 想顯示的字*/
 		return "redirect:/inquiry/form";
 	}/**/
